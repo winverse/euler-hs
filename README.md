e5b50d396a3d04b7938135b321574a81d3d4a4f4
winverse <public.winverse@gmail.com>
2022-05-06 18:23:49 +0900

    Solve Problem 2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# euler
+| A Haskell adventure into Euler Project
+
+# About euler project
+
+[euler project](https://projecteuler.net/about)
--- problems/001.hs
+++ problems/001.hs
@@ -3,4 +3,4 @@
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
 main :: IO ()
-main = print $ sum $ filter (\x -> mod x 3 == 0 || mod x 5 == 0 ) [1..1000]
+main = print $ sum $ filter (\x -> mod x 3 == 0 || mod x 5 == 0 ) [1..999]
