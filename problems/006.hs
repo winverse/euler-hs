a05fd12b5417896d7fc30ef29dcdedb8f4fb6993
sangHyun <public.winverse@gmail.com>
2022-09-06 07:14:02 +0900

    Solving the problem no.6

--- problems/006.hs
+++ problems/006.hs
@@ -1,3 +1,8 @@
+-- The sum of the squares of the first ten natural numbers is,
+-- The square of the sum of the first ten natural numbers is,
+-- Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+-- Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 square :: Int -> Int
 square n = n ^ 2
 
